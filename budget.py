373e775577ecae4de4e141a3007bf0b373ed8d79
mofresh27 <81204352+mofresh27@users.noreply.github.com>
2021-05-01 02:27:51 -0700

    Add files via upload

---
+++ budget.py
@@ -0,0 +1,39 @@
+#budget.py
+
+class Budget:
+
+    
+    def __init__(self, category, amount):
+        self.category = category
+        self.amount = amount
+    #methods
+    def deposit(self):
+        pass
+
+    def check_balance(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def transfer(self):
+        pass
+
+
+class Category(Budget):
+    self.category = {category1, category2, category3, category4, category5}
+
+    category1 = Budget("Clothing",200)
+    category2 = Budget("Bills",4000)
+    category3 = Budget("Groceries",500)
+    category4 = Budget("Dinning Out",200)
+    category5 = Budget("Entertainment",300)
+
+
+print(category.Buget())
+
+#print(type(Budget))
+
+
+# obj = Budget()
+# print(type(obj)
